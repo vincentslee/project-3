9066c453629eb7eff8dc0155787f310978fb1b06
John Glen <jglendening11@gmail.com>
2020-11-07 13:20:42 -0600

    edits

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,65 +5,112 @@ import Footer from '../components/footer';
 import IconImage from '../components/iconImage';
 import '../static/css/pages.css';
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Jumbotron phrase="From Beer to Code." subtitle="The story of my transition from Ballast Point Brewing Company to software development" />
-    <h3>My Story</h3>
-    <hr />
-    <div className="row">
-      <div className="col-md-6 aboutSections">
-        <img className="profilePic profilePic1" src="../static/pics/DSC_6082.jpg" alt="profilepic" />
-      </div>
-      <div className="col-md-6 aboutSections">
-        <h4 className="bio bio1">
-          Full-Stack fanatic who converts coffee into code.
-          My previous career as a Filter Technician at Ballast
-          Point Brewing Company revealed my passion for mechanical processes,
-          problem solving, and creating quality products through collaborative teamwork.
-          I specialize in HTML, CSS, JavaScript, and React on the front-end as well as Node
-          and MySQL/Mongo on the back-end to create beautiful and secure applications.
-          My biggest priority is developing clear, high-quality code while cultivating
-          a fun and encouraging workplace culture.
-        </h4>
-      </div>
-    </div>
-    <h3 className="skills">My Skills</h3>
-    <hr />
-    <div className="row">
-      <div className="col">
-        <div className="row">
-          <div className="col-sm-6">
-            <IconImage source="../static/pics/html.png" alt="html" />
-            <IconImage source="../static/pics/bootstrap.png" alt="bootstrap" />
-            <IconImage source="../static/pics/css.png" alt="css" />
-          </div>
-          <div className="col-sm-6">
-            <IconImage source="../static/pics/githubIcon.png" alt="github" />
-            <IconImage source="../static/pics/handlebars.png" alt="handlebars" />
-            <IconImage source="../static/pics/javascript.png" alt="javascript" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col">
-        <div className="row">
-          <div className="col-sm-6">
-            <IconImage source="../static/pics/jquery.png" alt="jquery" />
-            <IconImage source="../static/pics/mern.jpg" alt="mern" />
-            <IconImage source="../static/pics/mongodb.png" alt="mongodb" />
-          </div>
-          <div className="col-sm-6">
-            <IconImage source="../static/pics/mysql.png" alt="mysql" />
-            <IconImage source="../static/pics/nodejs.png" alt="node" />
-            <IconImage source="../static/pics/React.png" alt="react" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <Footer />
-  </div>
+const App = () => ( <
+    div >
+    <
+    Navbar / >
+    <
+    Jumbotron phrase = "Get a simple update on your health."
+    subtitle = "Get healthy, fast" / >
+    <
+    h3 > My Story < /h3> <
+    hr / >
+    <
+    div className = "row" >
+    <
+    div className = "col-md-6 aboutSections" >
+    <
+    img className = "profilePic profilePic1"
+    src = "../static/pics/DSC_6082.jpg"
+    alt = "profilepic" / >
+    <
+    /div> <
+    div className = "col-md-6 aboutSections" >
+    <
+    h4 className = "bio bio1" >
+    A biological information application -
+    User can record various necessary biological information and show medical professionals at the time of an emergency or a scheduled appointment.Features: Record weight, blood pressure, blood sugar, medication list, dietary journal Ideas
+    for breakdown of features:
+    -Use a library to create graphs
+    for our data -
+    Research api / library to create a pie chart to show nutritional information breakdown -
+    Notify the user to record information like blood sugar / pressure -
+    Email notification to record data and have the info recorded through a reply email to the app. <
+    /h4> <
+    /div> <
+    /div> <
+    h3 className = "skills" > Features < /h3> <
+    hr / >
+    <
+    div className = "row" >
+    <
+    div className = "col" >
+    <
+    div className = "row" >
+    <
+    div className = "col-sm-6" >
+    <
+    IconImage source = "../static/pics/html.png"
+    alt = "html" / >
+    <
+    IconImage source = "../static/pics/bootstrap.png"
+    alt = "bootstrap" / >
+    <
+    IconImage source = "../static/pics/css.png"
+    alt = "css" / >
+    <
+    /div> <
+    div className = "col-sm-6" >
+    <
+    IconImage source = "../static/pics/githubIcon.png"
+    alt = "github" / >
+    <
+    IconImage source = "../static/pics/handlebars.png"
+    alt = "handlebars" / >
+    <
+    IconImage source = "../static/pics/javascript.png"
+    alt = "javascript" / >
+    <
+    /div> <
+    /div> <
+    /div> <
+    /div> <
+    div className = "row" >
+    <
+    div className = "col" >
+    <
+    div className = "row" >
+    <
+    div className = "col-sm-6" >
+    <
+    IconImage source = "../static/pics/jquery.png"
+    alt = "jquery" / >
+    <
+    IconImage source = "../static/pics/mern.jpg"
+    alt = "mern" / >
+    <
+    IconImage source = "../static/pics/mongodb.png"
+    alt = "mongodb" / >
+    <
+    /div> <
+    div className = "col-sm-6" >
+    <
+    IconImage source = "../static/pics/mysql.png"
+    alt = "mysql" / >
+    <
+    IconImage source = "../static/pics/nodejs.png"
+    alt = "node" / >
+    <
+    IconImage source = "../static/pics/React.png"
+    alt = "react" / >
+    <
+    /div> <
+    /div> <
+    /div> <
+    /div> <
+    Footer / >
+    <
+    /div>
 );
 
 export default App;
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,45 +5,68 @@ import Picture from '../components/picture';
 import Footer from '../components/footer';
 import '../static/css/pages.css';
 
-const Home = () => (
-  <div>
-    <Navbar />
-    <Jumbotron phrase="From Front to Back." subtitle="A list of full stack, front end, and back end projects I worked on" />
-    <h3>Full Stack Applications</h3>
-    <hr />
-    <div className="row">
-      <div className="col-md-6">
-        <Picture source="../static/pics/sdbeer.png" alt="sdbeer" deployed="https://www.sdbeer.com/" />
-      </div>
-      <div className="col-md-6">
-        <Picture source="../static/pics/jobApp.png" alt="whistlr" deployed="https://jobothehunt.herokuapp.com/" />
-      </div>
-    </div>
-    <h3 className="frontend">Front End Websites</h3>
-    <hr />
-    <div className="row">
-      <div className="col-md-6">
-        <Picture source="../static/pics/reactPortfolio.png" alt="portfolio" github="https://github.com/davidlatuno/react-portfolio" deployed="https://davidlatuno-portfolio.herokuapp.com/" />
-      </div>
-      <div className="col-md-6">
-        <Picture source="../static/pics/FaceYourself.png" alt="faceyourself" github="https://github.com/davidlatuno/FaceYourselfDL" deployed="https://davidlatuno.github.io/FaceYourselfDL//" />
-      </div>
-    </div>
-    <h3 className="backend">CLI Applications</h3>
-    <hr />
-    <div className="row">
-      <div className="col-md-4">
-        <Picture source="../static/pics/bash.png" alt="liri" github="https://github.com/davidlatuno/bash" />
-      </div>
-      <div className="col-md-4">
-        <Picture source="../static/pics/csharp-copy.png" alt="hangman" github="https://github.com/davidlatuno/c-sharp-trilogy-script" />
-      </div>
-      <div className="col-md-4">
-        <Picture source="../static/pics/csharp-delete.png" alt="hangman" github="https://github.com/davidlatuno/c-sharp-trilogy-script2" />
-      </div>
-    </div>
-    <Footer />
-  </div>
+const Home = () => ( <
+    div >
+    <
+    Navbar / >
+    <
+    Jumbotron phrase = "Optimize wellness and performance through blood tests, nutrition, and science."
+    subtitle = "A quick and easy way to stay on top of overall health and wellness." / >
+    <
+    h3 > Full Stack Applications < /h3> <
+    hr / >
+    <
+    div className = "row" >
+    <
+    div className = "col-md-6" >
+    <
+    Picture source = ""
+    alt = ""
+    deployed = "" / >
+    <
+    /div> <
+    div className = "col-md-6" >
+    <
+    Picture source = ""
+    alt = ""
+    deployed = ""
+    " /> < /
+    div >
+
+    <
+    /div>
+
+    <
+    h3 className = "backend" > CLI Applications < /h3> <
+    hr / >
+    <
+    div className = "row" >
+    <
+    div className = "col-md-4" >
+    <
+    Picture source = ""
+    alt = ""
+    github = "" / >
+    <
+    /div> <
+    div className = "col-md-4" >
+    <
+    Picture source = "g"
+    alt = ""
+    github = "" / >
+    <
+    /div> <
+    div className = "col-md-4" >
+    <
+    Picture source = ""
+    alt = ""
+    github = "" / >
+    <
+    /div> < /
+    div > <
+    Footer / >
+    <
+    /div>
 );
 
 export default Home;
