e420ad7217d3ce794a4a05763ecc624a2ff9b4aa
John Glen <jglendening11@gmail.com>
2020-11-10 19:46:25 -0600

    edits

--- src/_app.jsx
+++ src/_app.jsx
@@ -10,12 +10,11 @@ library.add(fab, fas);
 class MainApp extends App {
     render() {
         const { Component, pageProps } = this.props;
-        return ( <
-            Container >
+        return ( < Container >
             <
             Head >
             <
-            title > Optimization of Wellness < /title> <
+            title > Optimization of Wellness < /title>   <
             meta property = "og:title"
             content = "Wellness Optimization" / >
             <
@@ -34,32 +33,28 @@ class MainApp extends App {
             link rel = "stylesheet"
             href = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
             integrity = "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
-            crossOrigin = "anonymous" /
-            >
+            crossOrigin = "anonymous" / >
             <
             script src = "https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity = "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossOrigin = "anonymous" /
-            >
+            crossOrigin = "anonymous" / >
             <
             script src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
             integrity = "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            crossOrigin = "anonymous" /
-            >
+            crossOrigin = "anonymous" / >
             <
             script src = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
             integrity = "sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
-            crossOrigin = "anonymous" /
-            >
+            crossOrigin = "anonymous" / >
             <
-            /Head> <
+            /Head>   <
             Favicon url = "favicon(3).ico" / >
             <
             div className = "container mainContainer" >
             <
             Component {...pageProps }
-            /> < /
-            div > <
+            /> </div >
+            <
             /Container>
         );
     }
