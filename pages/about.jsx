14a95bf332149085026541506b1645ddc56c089e
John Glendening <66345555+JohnGlendening@users.noreply.github.com>
2020-11-12 22:24:59 -0600

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,106 +5,58 @@ import Footer from '../components/footer';
 import IconImage from '../components/iconImage';
 import '../static/css/pages.css';
 
-const App = () => ( <
-    div >
-    <
-    Navbar / >
-    <
-    Jumbotron phrase = "To your health."
-    subtitle = "Stay on top of health issues with easy to follow documentation" / >
-    <
-    h3 > About < /h3> <
-    hr / >
-    <
-    div className = "row" >
-    <
-    div className = "col-md-6 aboutSections" >
-    <
-    img className = "Pic Pic1"
-    src = ".jpg"
-    alt = "" / >
-    <
-    /div> <
-    div className = "col-md-6 aboutSections" >
-    <
-    h4 className = "bio bio1" >
-
-    <
-    /h4> < /
-    div > <
-    /div> <
-    h3 className = "skills" > Tools < /h3> <
-    hr / >
-    <
-    div className = "row" >
-    <
-    div className = "col" >
-    <
-    div className = "row" >
-    <
-    div className = "col-sm-6" >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    /div> <
-    div className = "col-sm-6" >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    /div> < /
-    div > <
-    /div> < /
-    div > <
-    div className = "row" >
-    <
-    div className = "col" >
-    <
-    div className = "row" >
-    <
-    div className = "col-sm-6" >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    IconImage source = ".jpg"
-    alt = "" / >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    /div> <
-    div className = "col-sm-6" >
-    <
-    IconImage source = "..png"
-    alt = "m" / >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    IconImage source = ".png"
-    alt = "" / >
-    <
-    /div> < /
-    div > <
-    /div> < /
-    div > <
-    Footer / >
-    <
-    /div>
+const App = () => (
+  <div>
+    <Navbar />
+    <Jumbotron phrase="to your health" />
+    <h3>about</h3>
+    <hr />
+    <div className="row">
+      <div className="col-md-6 aboutSections">
+        <img className="profilePic profilePic1" src=".jpg" alt="profilepic" />
+      </div>
+      <div className="col-md-6 aboutSections">
+        <h4 className="bio bio1">
+        Talk about site
+        </h4>
+      </div>
+    </div>
+    <h3 className="tools">Tools</h3>
+    <hr />
+    <div className="row">
+      <div className="col">
+        <div className="row">
+          <div className="col-sm-6">
+            <IconImage source="../static/pics/html.png" alt="html" />
+            <IconImage source="../static/pics/bootstrap.png" alt="bootstrap" />
+            <IconImage source="../static/pics/css.png" alt="css" />
+          </div>
+          <div className="col-sm-6">
+            <IconImage source="../static/pics/githubIcon.png" alt="github" />
+            <IconImage source="../static/pics/handlebars.png" alt="handlebars" />
+            <IconImage source="../static/pics/javascript.png" alt="javascript" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <div className="row">
+          <div className="col-sm-6">
+            <IconImage source="../static/pics/jquery.png" alt="jquery" />
+            <IconImage source="../static/pics/mern.jpg" alt="mern" />
+            <IconImage source="../static/pics/mongodb.png" alt="mongodb" />
+          </div>
+          <div className="col-sm-6">
+            <IconImage source="../static/pics/mysql.png" alt="mysql" />
+            <IconImage source="../static/pics/nodejs.png" alt="node" />
+            <IconImage source="../static/pics/React.png" alt="react" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
 );
 
 export default App;
