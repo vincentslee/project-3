f1ad2d5b617cafb5cf35144e640fe78564fa49d6
John Glendening <66345555+JohnGlendening@users.noreply.github.com>
2020-11-12 22:12:20 -0600

    Update navbar.jsx

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,53 +2,29 @@ import Link from 'next/link';
 
 const Navbar = () => ( <
     nav className = "navbar navbar-expand-lg navbar-light bg-light" >
-    <
-    Link href = "/" >
-    <
-    a className = "navbar-brand" > To Your Health < /a> <
-    /Link> <
-    button className = "navbar-toggler"
+    <Link href = "/" >
+    <a className = "navbar-brand" > To Your Health < /a> 
+    </Link> <button className = "navbar-toggler"
     type = "button"
     data - toggle = "collapse"
     data - target = "#navbarSupportedContent"
     aria - controls = "navbarSupportedContent"
     aria - expanded = "false"
     aria - label = "Toggle navigation" >
-    <
-    span className = "navbar-toggler-icon" / >
-    <
-    /button>
+    <span className = "navbar-toggler-icon" / >
+    </button>
 
-    <
-    div className = "collapse navbar-collapse justify-content-end"
+    <div className = "collapse navbar-collapse justify-content-end"
     id = "navbarSupportedContent" >
-    <
-    ul className = "navbar-nav" >
-    <
+    <ul className = "navbar-nav" >
+    <li className = "nav-item" >
+    <Link href = "/" >
+    <a className = "nav-link" > Home < /a> </Link> </li> <li className = "nav-item" >
+    <Link href = "/about" >
+    <a className = "nav-link" > ABOUT < /a> </Link> </li> <
     li className = "nav-item" >
-    <
-    Link href = "/" >
-    <
-    a className = "nav-link" > Home < /a> <
-    /Link> <
-    /li> <
-    li className = "nav-item" >
-    <
-    Link href = "/about" >
-    <
-    a className = "nav-link" > ABOUT < /a> <
-    /Link> <
-    /li> <
-    li className = "nav-item" >
-    <
-    Link href = "/tools" >
-    <
-    a className = "nav-link" > Tools < /a> <
-    /Link> <
-    /li> <
-    /ul> <
-    /div> <
-    /nav>
+    <Link href = "/tools" >
+    <a className = "nav-link" > Tools < /a> </Link> </li> </ul> </div> </nav>
 );
 
 export default Navbar;
