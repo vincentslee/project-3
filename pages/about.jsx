ba6138ab720e01b48639d720fd1d700f7846b8db
John Glendening <66345555+JohnGlendening@users.noreply.github.com>
2020-11-14 09:43:43 -0600

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -36,24 +36,6 @@ const App = () => (
             <IconImage source="../static/pics/handlebars.png" alt="handlebars" />
             <IconImage source="../static/pics/javascript.png" alt="javascript" />
           </div>
-        </div>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col">
-        <div className="row">
-          <div className="col-sm-6">
-            <IconImage source="../static/pics/jquery.png" alt="jquery" />
-            <IconImage source="../static/pics/mern.jpg" alt="mern" />
-            <IconImage source="../static/pics/mongodb.png" alt="mongodb" />
-          </div>
-          <div className="col-sm-6">
-            <IconImage source="../static/pics/mysql.png" alt="mysql" />
-            <IconImage source="../static/pics/nodejs.png" alt="node" />
-            <IconImage source="../static/pics/React.png" alt="react" />
-          </div>
-        </div>
-      </div>
     </div>
     <Footer />
   </div>
