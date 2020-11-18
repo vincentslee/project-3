bf6d21b428110d7e9adcae54945b76a646225fbf
John Glendening <66345555+JohnGlendening@users.noreply.github.com>
2020-11-17 19:25:21 -0600

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,34 +5,21 @@ import Picture from '../components/picture';
 import Footer from '../components/footer';
 import '../static/css/pages.css';
 
-const Home = () => ( <
-    div >
-    <
-    Navbar / >
-    <
-    Jumbotron phrase = "Optimize wellness and performance through blood tests, nutrition, and science."
-import React from 'react';
-import Navbar from '../components/navbar';
-import Jumbotron from '../components/jumbotron';
-import Picture from '../components/picture';
-import Footer from '../components/footer';
-import '../static/css/pages.css';
-
 const Home = () => (
   <div>
     <Navbar />
-    <Jumbotron phrase="Optimize wellness and performance through blood tests, nutrition, and science." />
-    <h3>to your health</h3>
+    <Jumbotron phrase="Healthier." subtitle="A simpler way to stay healthy." />
+    <h3>In today's world, it can be difficult to stay up to speed on health issues. Let our application help you keep track.</h3>
     <hr />
     <div className="row">
       <div className="col-md-6">
-        <Picture source=".JPG" alt="Weight" deployed="https://johnglendening.github.io/" />
+        <Picture source="react-portfolio-master\static\pics\medical-graphs-8-728.jpg" alt="Graphs" deployed="" />
       </div>
       <div className="col-md-6">
-        <Picture source=".JPG" alt="E" deployed="" />
+        <Picture source="react-portfolio-master\static\pics\healthgraph1.png" alt="charts" deployed="" />
       </div>
       <div className="col-md-6">
-        <Picture source="r.JPG" alt="N" deployed="/" />
+        <Picture source="react-portfolio-master\static\pics\Section-of-medical-data-entry-form-example.png" alt="data entry logs" deployed="" />
       </div>
     </div>
     <Footer />
