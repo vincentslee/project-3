2f57fa4b540d9b28576ba5daa75dc4ac4ac61a71
John Glen <jglendening11@gmail.com>
2020-11-19 19:42:32 -0600

    additions

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,18 +9,21 @@ function Header(props) {
         return s.charAt(0).toUpperCase() + s.slice(1)
     }
 
-    let title = capitalize(props.location.pathname.substring(1,props.location.pathname.length))
-    if(props.location.pathname === '/') {
-        title = 'Welcome'
+    let title = capitalize(props.location.pathname.substring(1, props.location.pathname.length))
+    if (props.location.pathname === '/') {
+        title = 'Healthy Ways'
     }
 
     //Renders logout button if user is on home page
     function renderLogout() {
-        if(props.location.pathname === '/home'){
-            return(
-                <div className="ml-auto">
-                    <button className="btn btn-danger" onClick={() => handleLogout()}>Logout</button>
-                </div>
+        if (props.location.pathname === '/home') {
+            return ( <
+                div className = "ml-auto" >
+                <
+                button className = "btn btn-danger"
+                onClick = {
+                    () => handleLogout() } > Logout < /button> <
+                /div>
             )
         }
     }
@@ -32,13 +35,14 @@ function Header(props) {
 
 
     //JSX for component
-    return(
-        <nav className="navbar navbar-dark bg-primary">
-            <div className="row col-12 d-flex justify-content-center text-white">
-                <span className="h3">{props.title || title}</span>
-                {renderLogout()}
-            </div>
-        </nav>
+    return ( <
+        nav className = "navbar navbar-dark bg-primary" >
+        <
+        div className = "row col-12 d-flex justify-content-center text-white" >
+        <
+        span className = "h3" > { props.title || title } < /span> { renderLogout() } <
+        /div> <
+        /nav>
     )
 }
 export default withRouter(Header);
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,24 +1,24 @@
 /* eslint-disable */
-import React, {useEffect} from 'react';
-import {withRouter} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
 //import axios from 'axios';
 
-function Home(props){
+function Home(props) {
 
-    useEffect(()=>{
-//AXIOS POST REQUEST HERE
-/////////////////////////
+    useEffect(() => {
+        //AXIOS POST REQUEST HERE
+        /////////////////////////
     })
 
-    function redirectToLogin(){
+    function redirectToLogin() {
         props.history.push('/login');
     }
 
     //JSX component code. Currectly placeholder
-    return(
-        <div className="mt-2">
-            Home Page Content
-        </div>
+    return ( <
+        div className = "mt-2" >
+        Home Page Content <
+        /div>
     )
 }
 
